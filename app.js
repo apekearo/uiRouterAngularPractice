dcc924450be0334291bc90c223d94d56640c0416
apekearo <apekearo@gmail.com>
2019-01-02 15:38:17 -0600

    .

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/list',
             templateUrl: 'partial-home-list.html',
             controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                $scope.dogs = ['SheVerb', 'Dio', 'Yanni'];
             }
         })
         
         // nested list with just some random string data
         .state('home.paragraph', {
             url: '/paragraph',
-            template: 'I could sure use a drink right now.'
+            template: 'I love music! Even bad music.'
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
@@ -49,16 +49,16 @@ routerApp.controller('scotchController', function($scope) {
    
     $scope.scotches = [
         {
-            name: 'Macallan 12',
-            price: 50
+            name: 'Sheverb',
+            price: 100000
         },
         {
-            name: 'Chivas Regal Royal Salute',
+            name: 'Dio',
             price: 10000
         },
         {
-            name: 'Glenfiddich 1937',
-            price: 20000
+            name: 'Yanni',
+            price: 10
         }
     ];
     
